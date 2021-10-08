f2f237d52081de128e9eef168e93f5cbf287831b
Francisco Possetto <franciscopossetto@hotmail.com>
2021-10-08 13:37:42 +0200

    - added parameter validation. - added references to the revitapidocs. - renamed some methods to become more general the script..

--- WallExport.cs
+++ WallExport.cs
@@ -18,21 +18,45 @@ namespace ExportToExcel
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
             FilteredElementCollector collector = new FilteredElementCollector(doc);
-            ElementCategoryFilter FilterWalls = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
-            IList <Element> AllWalls = collector.WherePasses(FilterWalls).WhereElementIsNotElementType().ToElements();
+
+            // Replace this value to iterate on other categories.
+            BuiltInCategory builtInCategory = BuiltInCategory.OST_Walls;
+
+            ElementCategoryFilter elementCategoryFilter = new ElementCategoryFilter(builtInCategory);
+            
+            IList <Element> allElements = collector.WherePasses(elementCategoryFilter).WhereElementIsNotElementType().ToElements();
+
+            // Create an empty list to save all the elements that don´t have a value on the parameter width
+            List<Element> filteredElements = new List<Element>();
+            List<ElementId> filteredElementIds = new List<ElementId>();
 
 
-            List<Parameter> ElemWidthList = new List<Parameter>();
-           
-            foreach (Element e in AllWalls)
+            foreach (Element element in filteredElements)
             {
-                
-                ElemWidthList.Add(e.LookupParameter("Width"));
-                
+                string parameterName = "Width";
+                Parameter elementParameter = element.LookupParameter(parameterName);
 
+                // with this if, you check if the parameter exists on this element.
+                if (elementParameter != null)
+                {
+                    //Then you read the value
+                    string parameterValue = elementParameter.AsValueString();
+
+                // sometimes the value is not an string, so take a look on this posibilities, you may need to add a switch to identify the storage type before read it.
+
+                // https://www.revitapidocs.com/2022/0b04b80d-b318-986e-48cc-835d0dda76e5.htm
+
+                    if (parameterValue != "" || parameterValue != "")
+                    {
+                        filteredElements.Add(element); // you have all elements that need to fill this parameter.
+                        filteredElementIds.Add(element.Id); // or you could save the id directly.
+
+                        // or write a default value 
+                        // elementParameter.Set("default value");                    
+
+                    }
+                }
             }
-
-
 
             return Result.Succeeded;
 
