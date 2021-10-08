f8616e09c9c98a6241027f445e3e248faf880b4f
sunroads <fylypof@gmail.com>
2021-10-08 08:08:06 +0300

    Add project files.

---
+++ WallExport.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+
+namespace ExportToExcel
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class WallExport : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ElementCategoryFilter FilterWalls = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
+            IList <Element> AllWalls = collector.WherePasses(FilterWalls).WhereElementIsNotElementType().ToElements();
+
+
+            List<Parameter> ElemWidthList = new List<Parameter>();
+           
+            foreach (Element e in AllWalls)
+            {
+                
+                ElemWidthList.Add(e.LookupParameter("Width"));
+                
+
+            }
+
+
+
+            return Result.Succeeded;
+
+        }
+    }
+
+}
