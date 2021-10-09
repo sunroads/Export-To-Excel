9abc7ad015b4280419a0b2b8772bc1be1b59fc31
sunroads <fylypof@gmail.com>
2021-10-09 11:32:18 +0300

    .

--- WallExport.cs
+++ WallExport.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.Attributes;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
 
 namespace ExportToExcel
 {
@@ -21,16 +24,63 @@ namespace ExportToExcel
             ElementCategoryFilter FilterWalls = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
             IList <Element> AllWalls = collector.WherePasses(FilterWalls).WhereElementIsNotElementType().ToElements();
 
+            
+            List<string> AllWallsExcelKnows = new List<string>();
 
-            List<Parameter> ElemWidthList = new List<Parameter>();
-           
             foreach (Element e in AllWalls)
             {
+
+                AllWallsExcelKnows.Add(e.Name.ToString());
                 
-                ElemWidthList.Add(e.LookupParameter("Width"));
+            }
+
+            //List<Parameter> ElemWidthList = new List<Parameter>();
+
+            //foreach (Element e in AllWalls)
+            //{
+
+            //    ElemWidthList.Add(e.LookupParameter("Width"));
+
+
+            //}
+
+            using (var writer = new StreamWriter("C:\\Users\\b-filippov\\Desktop\\test\\Excel\\elements.csv"))
+
+
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                foreach (var s in AllWallsExcelKnows)
+                {
+                    csv.WriteField(s);
+                }
+
+                //writer.Flush();
                 
 
             }
+
+
+
+
+
+
+
+
+
+
+
+            //using (var csvWriter = new CsvHelper.CsvWriter(writer)
+            //{
+            //    foreach (string s in AllWallsExcelKnows)
+            //    {
+            //        csvWriter.WriteField(s);
+
+            //    }
+
+            //}
+
+
+
 
 
 
